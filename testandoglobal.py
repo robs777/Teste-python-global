2bda093cbbd39c4983f4a496d078f8f8e93a20bf
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-27 10:28:29 -0300

    parte 3

--- testandoglobal.py
+++ testandoglobal.py
@@ -20,9 +20,31 @@ def main():
         else:
             print(f'opção {paciente} inválida. Digite apenas sim ou não.')
 
+def sexo():
+    print("informe seu sexo biológico")
+    print ("M = masculino // F = feminino")
+    paciente = input("Digite M ou F: ")
+    if paciente == "M":
+        tipo_exameM()
+    elif paciente == "F":
+        tipo_exameF()
+
+    else:
+        print(f"opção {paciente} inválida. Digite apenas a letra M ou F")
 
 
-def tipo_exame():
+
+###################################
+
+
+#HOMEM
+
+
+###################################
+
+
+
+def tipo_exameM():
     print("Você gostaria de cadastrar quais tipos de exame?")
     print("1- Hemograma Completo")
     print("2- Glicemia")
@@ -66,7 +88,7 @@ def tipo_exame():
     else:
         print(f"Opção {paciente} inválida. Digite Novamente.")
 
-###################################
+
 def hemograma():
     print("Coloque os valores de cada requisito")
     print ("ERITROGRAMA")
@@ -77,25 +99,28 @@ def hemograma():
     volume_corp_médio = float(input("volume_corp_médio"))
     RDW = float(input("RDW"))
 
+    if hemacias == 5:
+        return "bom"
+
     print("LEUCOGRAMA")
-    leucocitos = float(input("Hemácias: "))
-    metamielócitos = float(input("Hemácias: "))
-    bastoneres = float(input("Hemácias: "))
-    segmentados = float(input("Hemácias: "))
-    eosinófilos = float(input("Hemácias: "))
-    basofilos = float(input("Hemácias: "))
-    linfocitos = float(input("Hemácias: "))
-    monocitos = float(input("Hemácias: "))
-    plaquetas = float(input("Hemácias: "))
+    leucocitos = float(input("leucocitos "))
+    metamielócitos = float(input("metamielócitos "))
+    bastoneres = float(input("bastoneres "))
+    segmentados = float(input("segmentados: "))
+    eosinófilos = float(input("eosinófilos: "))
+    basofilos = float(input("basofilos: "))
+    linfocitos = float(input("linfocitos: "))
+    monocitos = float(input("monocitos: "))
+    plaquetas = float(input("plaquetas: "))
 
 
 ########################################
 
 def glicemia():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 
 
@@ -105,70 +130,233 @@ def glicemia():
 
 def creatina():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 
 ########################################
 def acido_urico():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 
 ########################################
 def tgo():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 ########################################
     
 def tgp():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 
 ########################################
 def lipidograma():
-    colesterol = float(input("Hemácias: "))
-    hdl = float(input("Hemácias: "))
-    ldl = float(input("Hemácias: "))
-    vldl = float(input("Hemácias: "))
-    nhdl = float(input("Hemácias: "))
-    triglicerides = float(input("Hemácias: "))
+    colesterol = float(input("colesterol total: "))
+    hdl = float(input("colesterol HDL: "))
+    ldl = float(input("colesterol LDL: "))
+    vldl = float(input("colesterol VLDL: "))
+    nhdl = float(input("colesterol Não-HDL: "))
+    triglicerides = float(input("triglicerides: "))
 
 ########################################
 
 def hidroxivitamina():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 ########################################
 
 def tsh():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 
 ########################################
 
 def t4_livre():
     print("Coloque os valores de cada requisito")
-    resultado = float(input("Hemácias: "))
-    material = float(input("Hemácias: "))
-    metodo = float(input("Hemácias: "))
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
 ########################################
 def todos(): 
     pass
+
+
+###################################
+
+
+#MULHER
+
+
+###################################
+
+def tipo_exameF():
+    print("Você gostaria de cadastrar quais tipos de exame?")
+    print("1- Hemograma Completo")
+    print("2- Glicemia")
+    print("3- Creatina")
+    print("4- Ácido úrico")
+    print("5- Transaminase Glutamico Oxalacética - TGO")
+    print("6- Trasaminase Glutamico Pirúvica - TGP")
+    print("7- Lipidograma Completo")
+    print("8- 25-Hidroxivitamina D")
+    print("9- TSH Ultra Sensível")
+    print("10- T4 Livre")
+    print("11 - Opção para cadastrar todos os exames")
+
+    paciente = input("Digite sua escolha: ")
+
+    if paciente == "1": 
+        hemogramaF()
+
+    elif paciente =="2":
+        glicemiaF()
+
+    elif paciente =="3":
+        creatinaF()
+        
+    elif paciente =="4":
+        acido_uricoF()
+    elif paciente =="5":
+        tgoF()
+    elif paciente =="6":
+        tgpF()
+    elif paciente =="7":
+        lipidogramaF()
+    elif paciente =="8":
+        hidroxivitaminaF()
+    elif paciente =="9":
+        tshF()
+    elif paciente =="10":
+        t4_livreF()
+    elif paciente =="11":
+        todosF()
+    else:
+        print(f"Opção {paciente} inválida. Digite Novamente.")
+
+
+def hemogramaF():
+    print("Coloque os valores de cada requisito")
+    print ("ERITROGRAMA")
+    hemacias = float(input("Hemácias: "))
+    hemoglobina = float(input("hemoglobina"))
+    hematocrito = float(input("hematocrito"))
+    hb_corp_média = float(input("hb_corp_média"))
+    volume_corp_médio = float(input("volume_corp_médio"))
+    RDW = float(input("RDW"))
+
+    if hemacias == 5:
+        return "bom"
+
+    print("LEUCOGRAMA")
+    leucocitos = float(input("leucocitos "))
+    metamielócitos = float(input("metamielócitos "))
+    bastoneres = float(input("bastoneres "))
+    segmentados = float(input("segmentados: "))
+    eosinófilos = float(input("eosinófilos: "))
+    basofilos = float(input("basofilos: "))
+    linfocitos = float(input("linfocitos: "))
+    monocitos = float(input("monocitos: "))
+    plaquetas = float(input("plaquetas: "))
+
+
+########################################
+
+def glicemiaF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+
+
+########################################
+
+
+
+def creatinaF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+
+########################################
+def acido_uricoF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+
+########################################
+def tgoF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+########################################
+    
+def tgpF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+
+########################################
+def lipidogramaF():
+    colesterol = float(input("colesterol total: "))
+    hdl = float(input("colesterol HDL: "))
+    ldl = float(input("colesterol LDL: "))
+    vldl = float(input("colesterol VLDL: "))
+    nhdl = float(input("colesterol Não-HDL: "))
+    triglicerides = float(input("triglicerides: "))
+
+########################################
+
+def hidroxivitaminaF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+########################################
+
+def tshF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+
+########################################
+
+def t4_livreF():
+    print("Coloque os valores de cada requisito")
+    resultado = float(input("resultado: "))
+    material = float(input("material: "))
+    metodo = float(input("metodo: "))
+########################################
+def todosF(): 
+    pass
+
+
+
 
 
 
